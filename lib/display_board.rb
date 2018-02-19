b287fe86082d1d34fe72881e3968e45b0710f4f2
Ian  Jenkins <ianjerodjenkins@gmail.com>
2018-02-19 22:17:31 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 
 def display_board(board)
-board = [" 0 ", "  ", " X ", " 0 ", "  ", " X ", "  ", "  ", "  "]
-  puts "   |   |   "
+
+  puts "#{board[0]},#{board[1]},#{board[2]}" "   |   |   "
   puts "-----------"
-  puts "   |   |   "
+  puts "#{board[3]},#{board[4]},#{board[5]}""   |   |   "
   puts "-----------"
-  puts "   |   |   "
+  puts "#{board[6]},#{board[7]},#{board[8]}""   |   |   "
 
 
 end
